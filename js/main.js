142b1255f523a187b07373d5b211652c3285da1b
Pager6 <pager81020@gmail.com>
2025-05-25 20:53:44 +0500

    Адаптация 2 страниц и добавление JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(e) {
 let count = 1;
 const quantity = document.getElementById('quantity');
 
@@ -12,3 +13,25 @@ function decrease() {
         quantity.textContent = count;
     }
 }
+
+const form = document.getElementById('email-signup');
+const input = document.getElementById('email');
+
+const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function isEmailValid(value) {
+  return pattern.test(value);
+}
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.backgroundColor = '#b6e3bd';
+  } else {
+    input.style.backgroundColor = '#e3b6b6';
+    //e.preventDefault();
+  }
+}
+
+input.addEventListener('input', onInput);
+
+});
